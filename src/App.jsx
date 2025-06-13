4fdc5a3b07bcf7c1eff4f2df61acd1b0e8d15729
CappelliLeonardo <l.cappelli1995@gmail.com>
2025-06-13 10:14:32 +0200

    aggiunta modale

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,14 @@ function App() {
   }
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -37,7 +39,10 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
